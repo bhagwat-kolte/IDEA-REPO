bea7c44a97be6860431f2ee3804e99163ed974c8
Bhagwat Kolte <amol.kolte37@gmail.com>
2024-09-20 01:33:41 +0530

    first commit

---
+++ src/main/java/org/amol/basic/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.amol.basic;
+
+/**
+ * @author Bhagwat Kolte
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
