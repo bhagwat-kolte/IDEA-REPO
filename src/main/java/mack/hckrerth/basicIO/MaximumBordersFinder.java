4920ee5c29521fcbeadd97c9bfcf45f73bb008d0
Bhagwat Kolte <amol.kolte37@gmail.com>
2024-09-28 09:00:52 +0530

    Another set

---
+++ src/main/java/mack/hckrerth/basicIO/MaximumBordersFinder.java
@@ -0,0 +1,15 @@
+package mack.hckrerth.basicIO;
+
+/**
+ * @author Bhagwat Kolte
+ *
+ * You are given a table with n rows and m columns. Each cell is colored with white or black. Considering the shapes
+ * created by black cells, what is the maximum border of these shapes? Border of a shape means the maximum number of
+ * consecutive black cells in any row or column without any white cell in between. A shape is a set of connected
+ * cells. Two cells are connected if they share an edge. Note that no shape has a hole in it.
+ */
+public class MaximumBordersFinder {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/amol/controlFlow/NumberPalindromeChecker.java
@@ -0,0 +1,43 @@
+package org.amol.controlFlow;
+
+import java.util.Scanner;
+
+/**
+ * @author Bhagwat Kolte
+ *
+ * Java program to check number is Palindrome or not
+ */
+
+interface NumberChecker {
+    boolean isNumberPalindrome(Long num);
+}
+public class NumberPalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        Long number = scanner.nextLong();
+
+        NumberChecker numChk = (n) ->{
+            String num = "" + n;
+            StringBuilder num1 = new StringBuilder();
+            if(num.substring(0,1).equals("-")){
+                num1.append(num.substring(1));
+                num = num.substring(1);
+            }else{
+                num1.append(num);
+            }
+
+            String temp = num1.reverse().toString();
+
+            if(num.equalsIgnoreCase(temp))
+                return true;
+            else
+                return false;
+        };
+
+        if(numChk.isNumberPalindrome(number))
+            System.out.println("Given number " + number + " is Palindrome Number");
+        else
+            System.out.println("Given number " + number + " is not Palindrome Number");
+    }
+}
